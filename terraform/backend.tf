f46ede6f89eb8148d97a7d071024dbc8c2dd1be9
Paul Bruffett <paulbruffett@protonmail.com>
2026-04-25 08:51:47 -0700

    updated terraform login

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,7 @@
 terraform {
   backend "azurerm" {
     # Values provided via -backend-config in CI (see .github/workflows/terraform.yml).
-    # Bootstrap the state container once:
-    #   az group create -n tfstate-rg -l eastus2
-    #   az storage account create -n <unique> -g tfstate-rg -l eastus2 --sku Standard_LRS
-    #   az storage container create -n tfstate --account-name <unique>
-    use_oidc = true
+    # Auth via service principal client secret (ARM_CLIENT_ID/SECRET/TENANT_ID env vars).
+    use_azuread_auth = true
   }
 }
